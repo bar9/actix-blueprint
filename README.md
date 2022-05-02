a1fd14b596cd498c5ae5adcbd2611fccda0a6d8a
rb <r.brand@iwf.ch>
2022-05-02 19:48:11 +0200

    readme update and added https

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 #Actix-Blueprint
 
+## goals
+* an app blueprint capable of bootstrapping general-purpose web apps as well as APIs
+* lean by design: 
+  * no builds and preprocessing where it isn't absolutely necessary
+  * keep to web standards as closely as possible and use modern web APIs. 
+  * Stick to HTML, Javascript and CSS
+
+## maybe-goals
+* integration of mediasoup, e.g. in a dedicated branch
+
+## getting started
+* create a self-signed certificate (e.g with mkcert) under `cert/cert.pem` and `cert/key.pem`
+* build and run in one step with `cargo run`
+
 ## next steps
-- [ ] readme chapters goals, getting started
-- [ ] https
+- [x] readme chapters goals, getting started
+- [x] https
+- [ ] try a branch with mediasoup echo example
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use actix_web::{App, HttpServer, middleware, cookie::Key};
 use actix_files::{Files};
 use actix_session::SessionMiddleware;
 use actix_session::storage::CookieSessionStore;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[actix_web::main]
 async fn main() ->io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_intermediate(
+        SslMethod::tls()
+    ).unwrap();
+    builder.set_private_key_file("cert/key.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("cert/cert.pem").unwrap();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
@@ -14,7 +20,7 @@ async fn main() ->io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(Files::new("/", "static").index_file("index.html"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind_openssl("127.0.0.1:8080", builder)?
     .workers(2)
     .run()
     .await
